2f2251c48b187a66c77670f7b7cfe61cb4eb0cd2
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-26 09:18:17 +0100

    step 01 complete. need to reconfig array

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {
+    model: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    model: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    model: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    model: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
+
 class App extends Component {
+
+  state = {}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <select>
+          <option value="">--Pick a model--</option>
+          { 
+            data.map( index => 
+            <option key={ index.model } value={ index.model }>{ `${index.model} (${index.year})` }</option>)
+          }
+        </select>
       </div>
     );
   }
 }
 
 export default App;
+
+
